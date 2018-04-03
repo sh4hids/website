8605a30cd53c16c8a80ed76b695764e5b082a5b6
5h4hid5 <shahid.bracu@gmail.com>
2018-04-03 07:44:43 +0600

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# Personal Website &middot; ![reactjs](https://img.shields.io/badge/reactjs-16.2.0-blue.svg) ![license](https://img.shields.io/github/license/mashape/apistatus.svg)
--- src/components/molecules/HeroLogo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Card from '../atoms/Card';
-import Logo from '../atoms/Logo';
-
-const HeroLogo = () => (<Card bc="#21AAE2">
-  <Logo width="125" height="125" fill="none" stroke="#ffffff" strokeWidth="20" strokeMiterlimit="20"/>
-</Card>);
-
-export default HeroLogo;
